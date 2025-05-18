65cdd541261b704084a4839ddf4429c4a72b484a
Mark Karman <mkarman08@gmail.com>
2025-05-18 14:57:30 -0700

    Adding main file to scrape S&P 500

--- main.py
+++ main.py
@@ -3,3 +3,7 @@ import pandas as pd
 from indicators import bollinger_bands
 
 # Collect the S&P 500 stocks
+
+data = yf.Ticker("MSFT")
+df = data.history(period="3mo")
+print(bollinger_bands(df, plot=True))
