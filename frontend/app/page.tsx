2d9a619ad49ddb0bd6bad16256e47b8390482ea1
Mark Karman <mkarman08@gmail.com>
2026-01-01 20:41:34 -0800

    adding comment

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div className="flex flex-col min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <h1 className="text-2xl mb-3">Login To Trading Bot</h1>
+      {/* Adding a login authentication */}
       <form className="flex flex-col" action="send">
         <p>Login Email</p>
         <input type="email" id="user_email" className="border border-white-300 outline-none rounded-md border-white" />
