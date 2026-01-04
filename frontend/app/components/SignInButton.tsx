815e6f610b5af1a2aadb42a330bf249eeec88d43
Mark Karman <mkarman08@gmail.com>
2026-01-03 20:30:49 -0800

    adding sign in button component

---
+++ frontend/app/components/SignInButton.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import { signIn, signOut, useSession } from 'next-auth/react'
+
+export default function SignInButton() {
+  const { data: session } = useSession()
+
+  if (session) {
+    return (
+      <>
+        <p>Signed in as {session.user?.email}</p>
+        <button onClick={() => signOut()}>Sign Out</button>
+      </>
+    )
+  }
+  return <button onClick={() => signIn('google')}>Sign in with Google</button>
+}
