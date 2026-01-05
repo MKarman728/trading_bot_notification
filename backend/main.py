f40fe263f91d2c3d884952547229b570093f31be
Mark Karman <mkarman08@gmail.com>
2026-01-04 20:36:58 -0800

    enabled cors middleware for localhost

--- backend/main.py
+++ backend/main.py
@@ -6,14 +6,26 @@ import pandas as pd
 from indicators import bollinger_bands
 from dotenv import load_dotenv
 from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 from database import TradingDatabase
 from models import User
+import os
 
 # load dotenv files
 load_dotenv()
 
 # Sets up FastAPI end points
 app = FastAPI()
+
+FRONTEND_URL = os.getenv("FRONTEND_URL", "http://localhost:3000")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[FRONTEND_URL],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.post("/users")
